61f5293909e1e88d7bfffcbc3bbab039e7eb5179
woney23 <jiwonb683@gmail.com>
2024-11-26 09:03:27 +0000

    update 1126

--- chapter2_4.py
+++ chapter2_4.py
@@ -1,4 +1,8 @@
 import numpy as np
+import collections
+import sys
+sys.path.append('..')
+from np import * 
 
 
 class Embedding:
@@ -51,6 +55,48 @@ class EmbeddingDot:
         return dh
     
 
+class UnigramSampler:
+    def __init__(self, corpus, power, sample_size):
+        self.sample_size = sample_size
+        self.vocab_size = None
+        self.word_p = None
+
+        counts = collections.Counter()
+        for word_id in corpus:
+            counts[word_id] += 1
+
+        vocab_size = len(counts)
+        self.vocab_size = vocab_size
+
+        self.word_p = np.zeros(vocab_size)
+        for i in range(vocab_size):
+            self.word_p[i] = counts[i]
+
+        self.word_p = np.power(self.word_p, power)
+        self.word_p /= np.sum(self.word_p)
+
+    def get_negative_sample(self, target):
+        batch_size = target.shape[0]
+
+        if not GPU:
+            negative_sample = np.zeros((batch_size, self.sample_size), dtype=np.int32)
+
+            for i in range(batch_size):
+                p = self.word_p.copy()
+                target_idx = target[i]
+                p[target_idx] = 0
+                p /= p.sum()
+                negative_sample[i, :] = np.random.choice(self.vocab_size, size=self.sample_size, replace=False, p=p)
+        else:
+            # GPU(cupy）로 계산할 때는 속도를 우선한다.
+            # 부정적 예에 타깃이 포함될 수 있다.
+            negative_sample = np.random.choice(self.vocab_size, size=(batch_size, self.sample_size),
+                                               replace=True, p=self.word_p)
+
+        return negative_sample
 
 
-    
+class NegativeSamplingLoss:
+    def __init__(self, W, corpus, power=0.75, sample_size=5):
+        self.sample_size = sample_size
+        self.sampler = UnigramSampler(corpus, power, )
--- layers.py
+++ layers.py
@@ -289,3 +289,74 @@ class MatMul:
         dW = np.dot(self.x.T, dout)
         self.grads[0][...] = dW
         return dx
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+    
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0 # dW 형상은 유지한 채, 그 원소들을 0으로 덮어쓴다. 
+
+        # dW[self.idx] = dout #실은 나쁜 예
+        
+        # idx에서 중복을 처리할 수 있도록 수정
+        # for i, word_id in enumerate(self.idx):
+        #     dW[word_id]+=dout[i]
+        
+        np.add.at(dW, self.idx, dout) # 일반적으로 for문보다는 numpy 메서드를 사용하는 것이 효율이 높음.
+        return None
+        # 이렇게 하지 않고, 갱신하려는 행번호와 그 기울기를 따로 저장해두면, 이 정보로부터 가중치 W의 특정 행만 갱신할 수 있다.
+
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+    
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W*h,axis=1)
+
+        self.cache = (h,target_W)
+        return out
+    
+    def backward(self, dout):
+        h, target_W = self.cache
+        dout = dout.reshape(dout.shape[0],1)
+
+        dtarget_W = dout*h
+        self.embed.backward(dtarget_W)
+        dh = dout*target_W
+        return dh
+
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None
+        self.y = None  # sigmoid의 출력
+        self.t = None  # 정답 데이터
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - self.t) * dout / batch_size
+        return dx
+
