57f32d645565759bcc1a670890de6414e0522420
woney23 <jiwonb683@gamil.com>
2024-12-03 15:14:08 +0900

    update local

--- chapter2_4.py
+++ chapter2_4.py
@@ -3,6 +3,7 @@ import collections
 import sys
 sys.path.append('..')
 from np import * 
+from layers import *
 
 
 class Embedding:
@@ -99,9 +100,33 @@ class UnigramSampler:
 class NegativeSamplingLoss:
     def __init__(self, W, corpus, power=0.75, sample_size=5):
         self.sample_size = sample_size
-        self.sampler = UnigramSampler(corpus, power, )
-
-
-
-
+        self.sampler = UnigramSampler(corpus, power, sample_size)
+        self.loss_layers = [SigmoidWithLoss() for _ in range(sample_size+1)]
+        self.embed_dot_layers = [EmbeddingDot(W) for _ in range(sample_size +1)]
+        self.params, self.grads = [],[]
+        for layer in self.embed_dot_layers:
+            self.params += layer.params
+            self.grads += layer.grads
         
+    def forward(self, h, target):
+        batch_size = target.shape[0]
+        negative_sample = self.sampler.get_negative_sample(target)
+
+        #긍정적인 예 순전파
+        score = self.embed_dot_layers[0].forward(target)
+        correct_label = np.ones(batch_size, dtype=np.int32)
+        loss=self.loss_layers[0].forward(score, correct_label)
+
+        # 부정적인 예 순전파
+        negative_label = np.zeros(batch_size, dtype=np.int32)
+        for i in range(self.sample_size):
+            negative_target = negative_sample[:,i]
+            score = self.embed_dot_layers[1+i].forward(h, negative_target)
+            loss+=self.loss_layers[1+i].forward(score, negative_label)
+        return loss
+    
+    def backward(self,dout):
+           ddd
+
+
+
